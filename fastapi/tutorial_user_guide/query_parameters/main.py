ec5ec34a9d570f9df2beaee17a65862357ba8d63
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-02 23:13:58 +0100

    02-01-25: Optional query parameters

--- fastapi/tutorial_user_guide/query_parameters/main.py
+++ fastapi/tutorial_user_guide/query_parameters/main.py
@@ -24,5 +24,30 @@ items = [
 ]
 
 @app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
+async def read_items(skip: int = 0, limit: int = 10):
     return items[skip: skip + limit]
+
+#The query is the set of key value pair that goes after the ? and separated by & symbol
+#For example: http://127.0.0.1:8000/items/?skip=0&limit=10
+#So the query parameters are skip: with value of 0 and limit: with value of 10
+#They are naturally strings, since they are par of the URL
+#But when you declare them with Python types (in the example above, as int), 
+# they are converted to that type and validated against it.
+
+
+#Optional Parameters
+#The same way you can set optional value to query parameter by setting their default value to none
+#You can set optional parameters value too:
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    #This is how you set optional parameter q: str | None = None
+    """read item endpoint"""
+    if q:
+        print(q)
+        return {"item_id": item_id, "q": q}
+    print(q)
+    return {"item_id": item_id}
+#In this case, the function parameter q will be optional, and will be None by default.
+# Also notice that FastAPI is smart enough to notice that the path parameter item_id is a path parameter 
+# and q is not, so, it's a query parameter.
+
