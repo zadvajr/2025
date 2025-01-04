843bc5eb684a5bc80c4f4baac1fd05d3f66bdb8c
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-04 17:27:49 +0100

    04-01-25: insert into table

---
+++ py_mysql/insert_into_table.py
@@ -0,0 +1,22 @@
+#To fill a table in MySQL, use the "INSERT INTO" statement.
+# Example
+# Insert a record in the "customers" table:
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Sup@dm1n",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s %s)"
+val = ("John", "Highway 21")
+
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record added successfully!")
