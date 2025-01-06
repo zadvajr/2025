9f976ee779a7e981beb35383eb1d811ce392acfe
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-06 21:49:01 +0100

    06-01-25: Order By Descending

---
+++ py_mysql/order_by_descending.py
@@ -0,0 +1,21 @@
+#Order by desc
+#You can use the DESC keyword to sort the result in descending order.
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Sup@dm1n",
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM customers ORDER BY name DESC"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for result in myresult:
+    print(result)
