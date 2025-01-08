8945f30cf8dec431c2618acb8935c1667c04edc6
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-08 20:47:56 +0100

    08-01-25: Right join tables

---
+++ py_mysql/right_join.py
@@ -0,0 +1,23 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT \
+  users.name AS user, \
+  products.name AS favorite \
+  FROM users \
+  RIGHT JOIN products ON users.fav = products.id"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
