1e1f4303cc426accb28d45b6a54546350b1f553d
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-09 21:15:01 +0100

    09-01-25: Modified create db in mogodb

--- py_mongo_db/create_db.py
+++ py_mongo_db/create_db.py
@@ -1,7 +1,10 @@
 import pymongo
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+client = pymongo.MongoClient('mongodb://localhost:27017/')
 
-mydb = myclient['mydatabase']
+db = client['mydatabase']
 
 # database created!
+# Important: In MongoDB, a database is not created until it gets content!
+# MongoDB waits until you have created a collection (table), with at least one document (record) 
+# before it actually creates the database (and collection).
