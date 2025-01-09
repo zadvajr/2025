d3767a3abdff2f9b53724a6c63ddcfa472a88d10
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-09 21:54:50 +0100

    09-01-25: Modified create_db_collection

--- py_mongo_db/create_db_collection.py
+++ py_mongo_db/create_db_collection.py
@@ -1,9 +1,19 @@
+#Create DB Collection
+# A collection in MongoDB is the same as a table in SQL databases.
+# To create a collection in MongoDB, use database object and specify the name of the collection - 
+# you want to create.
+# MongoDB will create the collection if it does not exist.
+# Example
+# Create a collection called "customers":
 import pymongo
 
-myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+client = pymongo.MongoClient('mongodb://localhost:27017/')
 
-mydb = myclient['mydatabase']
+db = client['mydatabase']
 
-mycol = mydb["customers"]
+mycol = db["customers"]
 
 # collection created!
+# Important: In MongoDB, a collection is not created until it gets content!
+# MongoDB waits until you have inserted a document before it actually creates the collection.
+# MongoDB waits until you have inserted a document before it actually creates the collection.
