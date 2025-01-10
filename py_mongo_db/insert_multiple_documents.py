1e3d4a445a40973f73f0e912cbd050d329681c44
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-10 23:34:02 +0100

    10-01-25: Insert multiple data into collection

---
+++ py_mongo_db/insert_multiple_documents.py
@@ -0,0 +1,29 @@
+# Insert Multiple Documents
+# To insert multiple documents into a collection in MongoDB, we use the insert_many() method.
+# The first parameter of the insert_many() method is a list containing dictionaries
+# with the data you want to insert:
+# Example
+import pymongo
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client['mydatabase']
+mycol = db['customers']
+
+mylist = mylist = [
+  { "name": "Amy", "address": "Apple st 652"},
+  { "name": "Hannah", "address": "Mountain 21"},
+  { "name": "Michael", "address": "Valley 345"},
+  { "name": "Sandy", "address": "Ocean blvd 2"},
+  { "name": "Betty", "address": "Green Grass 1"},
+  { "name": "Richard", "address": "Sky st 331"},
+  { "name": "Susan", "address": "One way 98"},
+  { "name": "Vicky", "address": "Yellow Garden 2"},
+  { "name": "Ben", "address": "Park Lane 38"},
+  { "name": "William", "address": "Central st 954"},
+  { "name": "Chuck", "address": "Main Road 989"},
+  { "name": "Viola", "address": "Sideway 1633"}
+]
+
+x = mycol.insert_many(mylist)
+
+print(x)
