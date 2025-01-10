40b1a497dcf2e380b16aba0c6564bd0476a235ba
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-10 22:24:12 +0100

    10-01-25: insert data into collection

--- py_mongo_db/insert_into_collection.py
+++ py_mongo_db/insert_into_collection.py
@@ -1,3 +1,9 @@
+# A document in MongoDB is the same as a record in SQL databases.
+# To insert a record, or document as it is called in MongoDB, into a collection, we use the insert_one() method.
+# The first parameter of the insert_one() method is a dictionary containing the name(s) and value(s) -
+# of each field in the document you want to insert.
+# Example
+# Insert a record in the "customers" collection:
 import pymongo
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
@@ -6,6 +12,6 @@ mycol = db["customers"]
 
 mydict = { "name": "John", "address": "Highway 37" }
 
-x = mycol.insert_one(mydict)
+inserted_data = mycol.insert_one(mydict)
 
-print(x)
+print(inserted_data)
