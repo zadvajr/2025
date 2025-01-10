39d80138e18b46029c90f7a8f4744fd9cb710c0c
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-10 23:49:10 +0100

    10-01-25: insert multiple documents with specified id

---
+++ py_mongo_db/insert_multiple_documents_with_specified_id.py
@@ -0,0 +1,4 @@
+# Insert Multiple Documents, with Specified IDs
+# If you do not want MongoDB to assign unique ids for your document, 
+# you can specify the _id field when you insert the document(s).
+# Remember that the values has to be unique. Two documents cannot have the same _id.
