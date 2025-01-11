51c573429c21416c5926cef32ad24bc0e0c3376b
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-12 00:28:19 +0100

    12-01-25: exclude some fields from find()

--- py_mongo_db/exclude_field_from_find.py
+++ py_mongo_db/exclude_field_from_find.py
@@ -10,3 +10,11 @@ mycol = mydb["customers"]
 
 for x in mycol.find({},{ "address": 0 }):
   print(x)
+
+
+# Example
+# You get an error if you specify both 0 and 1 values in the same object 
+# (except if one of the fields is the _id field):
+# This section will produce an error
+# for x in mycol.find({},{ "name": 1, "address": 0 }):
+#   print(x)
