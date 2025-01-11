f2bee40e0b4dac28856601a9b7f105487dee16f0
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-11 04:56:13 +0100

    11-01-25: return only some fields with find method

--- py_mongo_db/return_only_some_fields.py
+++ py_mongo_db/return_only_some_fields.py
@@ -12,3 +12,7 @@ mycol = db["customers"]
 results = mycol.find({}, { "_id": 0, "name": 1, "address": 1 })
 for result in results:
   print(result)
+
+# You are not allowed to specify both 0 and 1 values in the same object 
+# (except if one of the fields is the _id field). If you specify a field with the value 0,
+# all other fields get the value 1, and vice versa:
