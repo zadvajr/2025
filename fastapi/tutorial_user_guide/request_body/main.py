6c9f10a01b6199a70da3e9d2df72c13e7cc9502d
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-12 01:38:45 +0100

    12-01-25: request body

---
+++ fastapi/tutorial_user_guide/request_body/main.py
@@ -0,0 +1,18 @@
+#Request Body
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Item(BaseModel):
+    """Item class model with pydantic"""
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    """create item"""
+    return item
+
