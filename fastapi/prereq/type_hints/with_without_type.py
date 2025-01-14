334a0aec259e91608b3dafe08c576983847d9339
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-14 20:50:40 +0100

    14-01-25: type hints

---
+++ fastapi/prereq/type_hints/with_without_type.py
@@ -0,0 +1,12 @@
+#Without int arguments
+
+# def total_price(price_1,price_2):
+#     return f"Your total bill is USD {price_1+price_2}"
+        
+# print(total_price(30,40)) # Output: 70
+
+#with string arguments
+def total_price(price_1,price_2):
+    return f"Your total bill is USD {price_1+price_2}"
+        
+print(total_price('30','40')) #Outputs 3040
