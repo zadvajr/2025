8d59a8e2dcc3272bc6cea885888ae08d4dfd21b4
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-17 00:20:06 +0100

    17-01-25: Read file contents line by lines

--- py_core/file_handling_in_python/readlines.py
+++ py_core/file_handling_in_python/readlines.py
@@ -6,3 +6,13 @@
 
 f = open("demofile.txt", "r")
 print(f.readline())
+
+# By calling readline() two times, you can read the two first lines:
+
+# Example
+# Read two lines of the file:
+
+f = open("demofile.txt", "r")
+print(f.readline())
+print(f.readline())
+
