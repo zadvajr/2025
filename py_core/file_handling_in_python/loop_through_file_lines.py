20fde65251b878b63e6e2602b560112617bfd5aa
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-17 00:20:56 +0100

    17-01-25: Read file contents line by lines

---
+++ py_core/file_handling_in_python/loop_through_file_lines.py
@@ -0,0 +1,8 @@
+# By looping through the lines of the file, you can read the whole file, line by line:
+
+# Example
+# Loop through the file line by line:
+
+f = open("demofile.txt", "r")
+for x in f:
+  print(x)
