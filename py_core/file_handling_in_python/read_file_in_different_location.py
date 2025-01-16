30a29d019ae3ba178eedb5f4372e438a6fb95a4c
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-16 23:00:50 +0100

    16-01-2025: Read file in different locations

---
+++ py_core/file_handling_in_python/read_file_in_different_location.py
@@ -0,0 +1,8 @@
+#Read file in different location
+# If the file is located in a different location, you will have to specify the file path, like this:
+
+# Example
+# Open a file on a different location:
+
+f = open("D:\\myfiles\welcome.txt", "r")
+print(f.read())
