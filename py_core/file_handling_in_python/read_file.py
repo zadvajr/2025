c65b61f4838fac0da4c732ba96155f712394f73c
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-16 22:58:18 +0100

    16-01-2025: Read file

---
+++ py_core/file_handling_in_python/read_file.py
@@ -0,0 +1,7 @@
+# Read File
+# To open the file, use the built-in open() function.
+# The open() function returns a file object, which has a read() method for reading the content of the file:
+
+# Example
+file = open("demofile.txt", "r")
+print(file.read())
