f490f5a245b450489365c3db67b3db31b7074c94
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-19 00:54:34 +0100

    19-01-25: built-in-modules

---
+++ py_core/python_modules/built_in_modules.py
@@ -0,0 +1,9 @@
+# Built-in Modules
+# There are several built-in modules in Python, which you can import whenever you like.
+# Example
+# Import and use the platform module:
+
+import platform
+
+x = platform.system()
+print(x)
