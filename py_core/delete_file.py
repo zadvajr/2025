7cd3baf7774bace8b2f06808f222da11ae83c239
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-18 20:53:34 +0100

    18-01-25: delete a file

---
+++ py_core/delete_file.py
@@ -0,0 +1,8 @@
+#Python: Delete Files
+# To delete a file, you must import the OS module, and run its os.remove() function:
+
+# Example
+# Remove the file "demofile3.txt":
+import os
+
+os.remove("demofile3.txt")
