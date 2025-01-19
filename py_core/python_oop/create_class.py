89022c4794ab39536e122de5bb557f520e0c2a64
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-19 23:45:47 +0100

    19-01-25: create an object of class

--- py_core/python_oop/create_class.py
+++ py_core/python_oop/create_class.py
@@ -12,4 +12,10 @@
 class MyClass:
   x = 5
 
-print(MyClass)
+# Create Object
+# Now we can use the class named MyClass to create objects:
+
+# Example
+# Create an object named p1, and print the value of x:
+p1 = MyClass()
+print(p1.x)
