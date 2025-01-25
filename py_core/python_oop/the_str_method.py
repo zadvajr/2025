3b8cd04d8183311be26900ca1690af9c7d1a159f
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-25 19:24:38 +0100

    25-01-25: The string method

--- py_core/python_oop/the_str_method.py
+++ py_core/python_oop/the_str_method.py
@@ -6,3 +6,16 @@ class Person:
 p1 = Person("John", 36)
 
 print(p1)
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
