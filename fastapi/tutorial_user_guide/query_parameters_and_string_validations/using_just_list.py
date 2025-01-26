351146e2217dd330c04872adecd6227f9afbda82
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-26 13:44:37 +0100

    26-05-25: Modified using_just_list.py file

--- fastapi/tutorial_user_guide/query_parameters_and_string_validations/using_just_list.py
+++ fastapi/tutorial_user_guide/query_parameters_and_string_validations/using_just_list.py
@@ -1,5 +1,6 @@
+"""Using Just list"""
+# You can also use list directly instead of List[str] (or list[str] in Python 3.9+):
 from typing import Annotated
-
 from fastapi import FastAPI, Query
 
 app = FastAPI()
@@ -10,3 +11,7 @@ async def read_items(q: Annotated[list, Query()] = []):
     """Using just lists"""
     query_items = {"q": q}
     return query_items
+
+# Keep in mind that in this case, FastAPI won't check the contents of the list.
+# For example, List[int] would check (and document) that the contents of the list are integers.
+# But list alone wouldn't.
