2ecf77e30b68e818422cd1e584da8c957382de13
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-29 00:00:47 +0100

    29-01-25: body multiple parameters

---
+++ fastapi/tutorial_user_guide/body_multiple_parameters/main.py
@@ -0,0 +1,8 @@
+"""Body - Multiple Parameters"""
+# Now that we have seen how to use Path and Query,
+# let's see more advanced uses of request body declarations.
+# Mix Path, Query and body parameters
+
+# First, of course, you can mix Path, Query and request body parameter declarations freely
+# and FastAPI will know what to do.
+# And you can also declare body parameters as optional, by setting the default to None:
