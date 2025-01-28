b5ee56281643a72e7371daf53fb9cd33c0953c26
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-28 22:58:54 +0100

    28-01-25: Modified query_parameters_models

--- fastapi/tutorial_user_guide/query_parameters_models/main.py
+++ fastapi/tutorial_user_guide/query_parameters_models/main.py
@@ -1,10 +1,12 @@
 """Query Parameter Models"""
-# If you have a group of query parameters that are related, you can create a Pydantic model to declare them.
-# This would allow you to re-use the model in multiple places and also to declare validations
-# and metadata for all the parameters at once.
+#If you have a group of query parameters that are related,
+# you can create a Pydantic model to declare them.
+#This would allow you to re-use the model in multiple places and also to declare validations
+#and metadata for all the parameters at once.
 
 # Query Parameters with a Pydantic Model
-# Declare the query parameters that you need in a Pydantic model, and then declare the parameter as Query:
+# Declare the query parameters that you need in a Pydantic model,
+# and then declare the parameter as Query:
 
 from typing import Annotated, Literal
 from fastapi import FastAPI, Query
@@ -12,14 +14,14 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-
 class FilterParams(BaseModel):
-    limit: int = Field(100, gt=0, le=100)
+    """Query Param Model"""
+    limit: int = Field(100, ge=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
     tags: list[str] = []
 
-
 @app.get("/items/")
 async def read_items(filter_query: Annotated[FilterParams, Query()]):
+    """Read Items EndPoint"""
     return filter_query
