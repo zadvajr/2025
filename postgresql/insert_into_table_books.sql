bf490a3a8975853c3e0b9ee430f44442b60b565f
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-30 02:20:41 +0100

    30-01-25: insert into table books

---
+++ postgresql/insert_into_table_books.sql
@@ -0,0 +1,28 @@
+INSERT INTO books(id, title, isbn, author, publisher, price)
+VALUES
+(
+'e5b27988-5bda-4cd4-9593-4ab7f513bd7d',
+'The Richest Man in Babylon',
+'0452267250',
+'George S. Classon',
+'Plume Books',
+2000
+),
+(
+'fb050200-c070-4fa4-b6fb-561440e31ec5',
+'Rich Dad Poor Dad',
+'0446611093',
+'Robert T. Kiyosaki',
+'Richdads Books',
+3500
+),
+(
+'8826586b-ee4a-4664-ad32-30d7913265dd',
+'Why So Hot?',
+'9789787890457',
+'Dr. Jumoke Aleoke-Malachi',
+'Solutions by JAM',
+5000
+);
+
+-- SELECT * FROM books
