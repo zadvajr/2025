422cc2533c37077058de6eeece1e7dcbdf4e84e9
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-30 02:04:51 +0100

    30-01-25: insert into table authors

---
+++ postgresql/insert_into_table_authors.sql
@@ -0,0 +1,18 @@
+INSERT INTO authors(id, first_name, last_name) 
+VALUES(
+'8889cd9a-5c72-4a8f-bc4b-778ac5ecada6',
+'Robert',
+'Kiyosaki'
+),
+(
+'96cfc7d9-2d33-452a-aa5c-fbeb983ad923',
+'George',
+'Classon'
+),
+(
+'f91ea5a7-38ff-4f78-b4e9-89359286f6b9',
+'Brian',
+'Tracy'
+);
+
+-- SELECT * FROM authors
