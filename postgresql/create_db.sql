e396094aee4811a6cbf54faa66f97180e260659f
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-31 23:10:03 +0100

    31-01-25: creating db

---
+++ postgresql/create_db.sql
@@ -0,0 +1,17 @@
+-- Database: bookstore
+
+-- DROP DATABASE IF EXISTS bookstore;
+
+CREATE DATABASE bookstore
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE bookstore
+    IS 'bookstore_db';
