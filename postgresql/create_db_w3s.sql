13f7b6fa1c4d49c9f9e6f088eb85872cd33b9797
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-01-31 23:11:30 +0100

    31-01-25: creating db w3s

---
+++ postgresql/create_db_w3s.sql
@@ -0,0 +1,17 @@
+-- Database: w3s
+
+-- DROP DATABASE IF EXISTS w3s;
+
+CREATE DATABASE w3s
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE w3s
+    IS 'w3schools.com';
