b35b3a78de9e67764f706acfacad91c590d8f236
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-03 23:48:21 +0100

    03-02-25: create_table_cars

---
+++ postgresql/create_table_cars.sql
@@ -0,0 +1,7 @@
+-- Create Table
+-- The following SQL statement will create a table named cars in your PostgreSQL database: w3s
+CREATE TABLE cars (
+    brand VARCHAR(255),
+    model VARCHAR(255),
+    year INT
+);
