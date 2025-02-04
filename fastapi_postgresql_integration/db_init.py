5c6811841c3c6193defd90b85bd06dec3bccf760
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-04 21:48:52 +0100

    05-02-25: added db_init.py

---
+++ fastapi_postgresql_integration/db_init.py
@@ -0,0 +1,10 @@
+import asyncio
+from database import engine
+from models import Base
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
