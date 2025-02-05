ef7ca37ca18dd9efe2670f2481e7724205e46c6a
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-05 23:45:38 +0100

    05-02-25: modified db url

--- fastapi_postgresql_integration/config.py
+++ fastapi_postgresql_integration/config.py
@@ -1,1 +1,1 @@
-DATABASE_URL = "postgresql+asyncpg://postgres:Super@dm1n@localhost:5432/fastapi_postgresql_integration_db"
+DATABASE_URL = "postgresql+asyncpg://postgres:Super%40dm1n@localhost:5432/fastapi_postgresql_integration_db"
