4242edb7c5f3ad0bcf48b25b0c0e3280dcc3bbe1
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-05 23:57:02 +0100

    05-02-25: mino fix

--- fastapi_postgresql_integration/main.py
+++ fastapi_postgresql_integration/main.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from pydantic import BaseModel
+# from pydantic import BaseModel
 from database import get_db
 from models import User
 
 app = FastAPI()
 
-class Users(BaseModel):
-    name: str
-    email: str
+# class Users(BaseModel):
+#     name: str
+#     email: str
 
 @app.get("/")
 async def root():
     return {"message": "FastAPI + PostgreSQL"}
 
 @app.post("/users/")
-async def create_user(user: Users, db: AsyncSession = Depends(get_db)):
-    new_user = User(name=user.name, email=user.email)
+async def create_user(name: str, email: str, db: AsyncSession = Depends(get_db)):
+    new_user = User(name=name, email=email)
     db.add(new_user)
     await db.commit()
     return {"message": "User created successfully"}
