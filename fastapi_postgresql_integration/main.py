781b85c48f9ca9d97fc3c1616093ee65c801bccd
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-05 23:29:35 +0100

    05-02-25: modified main.py

--- fastapi_postgresql_integration/main.py
+++ fastapi_postgresql_integration/main.py
@@ -8,14 +8,13 @@ from models import User
 app = FastAPI()
 
 class Users(BaseModel):
-    name: str,
-    email: str,
-    db: AsyncSession = Depends(get_db)
+    name: str
+    email: str
 
 @app.get("/")
 async def root():
     return {"message": "FastAPI + PostgreSQL"}
 
 @app.post("/users/")
-async def create_user(user: Users):
+async def create_user(user: Users, db: AsyncSession = Depends(get_db)):
     new_user = User(name=user.name, email=user.email)
