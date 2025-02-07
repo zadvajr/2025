c53f420d62914c28dcab9c1f9640571b88dc08a6
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-07 13:05:27 +0100

    07-02-25: selecting data from a database table

---
+++ postgresql/select_statement.sql
@@ -0,0 +1,12 @@
+-- Select Data
+-- To retrieve data from a database, we make use of SELECT statement
+
+-- Specify Columns
+-- by specifying column name we can choose which column to select
+-- Example
+SELECT brand, model FROM cars; -- will select only brand and model column from cars table
+
+-- Return All Columns
+-- To select all columns from a table, we make use of * instead of column names
+--Example
+SELECT * FROM cars;
