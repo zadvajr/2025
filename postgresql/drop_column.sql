13022880186c6e3fa69a67b5d72aeef9022da422
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-08 00:24:28 +0100

    08-02-25: drop table

---
+++ postgresql/drop_column.sql
@@ -0,0 +1,7 @@
+-- Drop Column
+-- we want to remove the column named color from the cars table
+-- To remove a column use the DROP COLUMN statement
+ALTER TABLE cars
+DROP COLUMN color;
+
+-- Result: ALTER TABLE
