e5a7d116d1a9d12baf0cb3b1387ad1d6e751bffa
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-08 00:35:45 +0100

    08-02-25: delete all records in a statement

---
+++ postgresql/delete_all_records.sql
@@ -0,0 +1,11 @@
+-- Delete All Records
+-- It is possible to delete all rows in a table without deleting the table.
+-- This means that the table structure, attributes, and indexes will be intact.
+
+-- The following SQL statement deletes all rows in the cars table, without deleting the table:
+
+-- Example
+-- Delete all records in the cars table:
+DELETE FROM cars;
+
+-- Result: DELETE 2 - Which means 3 records are deleted
