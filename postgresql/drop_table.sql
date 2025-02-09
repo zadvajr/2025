a2524684940d50190a857ff7d58e0e60c61997b1
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-09 23:46:30 +0100

    09-02-25: drop a table

--- postgresql/drop_table.sql
+++ postgresql/drop_table.sql
@@ -1,3 +1,8 @@
--- Drop Table named cars
+-- The DROP TABLE Statement
+-- The DROP TABLE  statement is used to drop a table in a database
+-- Note: Be careful before dropping a table.
+-- Deleting a table will result in loss of all information stored in the table!
 
+--Example
+-- Drop Table named cars
 DROP TABLE cars;
