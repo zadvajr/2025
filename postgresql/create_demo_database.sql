df1eb83dbfd4910c060f0bc96bf69d9264cbf8a2
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-10 02:50:03 +0100

    insert into testproducts table

--- postgresql/create_demo_database.sql
+++ postgresql/create_demo_database.sql
@@ -3294,3 +3294,17 @@ CREATE TABLE testproducts (
 -- We will fill the table with 10 dummy products:
 
 -- INSERT INTO testproducts
+INSERT INTO testproducts (product_name, category_id)
+VALUES
+  ('Johns Fruit Cake', 3),
+  ('Marys Healthy Mix', 9),
+  ('Peters Scary Stuff', 10),
+  ('Jims Secret Recipe', 11),
+  ('Elisabeths Best Apples', 12),
+  ('Janes Favorite Cheese', 4),
+  ('Billys Home Made Pizza', 13),
+  ('Ellas Special Salmon', 8),
+  ('Roberts Rich Spaghetti', 5),
+  ('Mias Popular Ice', 14);
+
+-- Result: INSERT 0 10
