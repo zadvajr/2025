eed8221572cf704e0f62139988856951c07c2b8b
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-11 23:45:28 +0100

    11-02-2025: less than or equal to

---
+++ postgresql/less_than_or_equal_to.sql
@@ -0,0 +1,8 @@
+-- Less Than or Equal To
+-- The <= operator is used when you want to return all records where a column is less than, or equal to, a specified value.
+
+-- Example
+-- Return all records where the year is less than or equal to 1975:
+
+SELECT * FROM cars
+WHERE year <= 1975;
