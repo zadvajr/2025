24ed428ee9bf0ecfcf57ef638b8457ec9c38f442
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-11 23:42:41 +0100

    11-02-2025: less than operator

---
+++ postgresql/less_than.sql
@@ -0,0 +1,8 @@
+-- Less Than
+-- The < operator is used when you want to return all records where a column is less than a specified value.
+
+-- Example
+-- Return all records where the year is less than 1975:
+
+SELECT * FROM cars
+WHERE year < 1975;
