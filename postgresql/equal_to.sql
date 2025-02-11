741d48891d070abf3339c3fe84a3eb4bd3ae238e
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-11 23:31:42 +0100

    11-02-2025: equal_to operator

---
+++ postgresql/equal_to.sql
@@ -0,0 +1,7 @@
+-- Equal To
+-- The = operator is used when you want to return all records where a column is equal to a specified value:
+
+-- Example
+-- Return all records where the brand is 'Volvo':
+SELECT * FROM cars
+WHERE brand = 'Volvo';
