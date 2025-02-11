eb87918fc126a98a6b2020f4e2fe747a607e7239
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-11 23:44:20 +0100

    11-02-2025: greater than operator

---
+++ postgresql/greater_than_operator.sql
@@ -0,0 +1,8 @@
+-- Greater Than
+-- The > operator is used when you want to return all records where a columns is greater than a specified value.
+
+-- Example
+-- Return all records where the year is greater than 1975:
+
+SELECT * FROM cars
+WHERE year > 1975;
