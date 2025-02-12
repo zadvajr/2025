b36f614b5ad3295d24e0b566122e07a7a0a3074f
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-12 23:37:28 +0100

    12-02-2025: filter record

---
+++ postgresql/filter_record.sql
@@ -0,0 +1,10 @@
+-- Filter Records
+-- The WHERE clause is used to filter records.
+
+-- It is used to extract only those records that fulfill a specified condition.
+
+-- If we want to return only the records where city is London, we can specify that in the WHERE clause:
+
+-- Example
+SELECT * FROM customers
+WHERE city = 'London';
