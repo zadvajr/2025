b3a780e9bd8b504c16a03a5689f397330ff67292
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-15 23:35:28 +0100

    15-02-2025: sort alphabetically

---
+++ postgresql/sort_alphabetically.sql
@@ -0,0 +1,8 @@
+-- Sort Alphabetically
+-- For string values the ORDER BY keyword will order alphabetically:
+
+-- Example
+-- Sort the table by product name:
+
+SELECT * FROM products
+ORDER BY product_name;
