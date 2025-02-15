3349c3c5d157dda1521cc250992f0c388b5f010d
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-15 23:29:39 +0100

    15-02-2025: sorting data

---
+++ postgresql/sorting_data.sql
@@ -0,0 +1,10 @@
+-- ORDER BY Statement
+-- Sort Data
+-- The ORDER BY keyword is used to sort the result in ascending or descending order.
+-- The ORDER BY keyword sorts the records in ascending order by default.
+-- To sort the records in descending order, use the DESC keyword.
+
+-- Example
+-- Sort the table by price:
+SELECT * FROM products
+ORDER BY price;
