0447df1798b2b59d1b7508ef6091eb21fe90e65f
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-15 23:41:05 +0100

    15-02-2025: limit clause

---
+++ postgresql/limit_clause.sql
@@ -0,0 +1,8 @@
+-- The LIMIT Clause
+-- The LIMIT clause is used to limit the maximum number of records to return.
+
+-- Example
+-- Return only the 20 first records from the customers table:
+
+SELECT * FROM customers
+LIMIT 20;
