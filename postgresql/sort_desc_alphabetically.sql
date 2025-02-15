742cae499fdfe90b017c6046196b9cbc4a29bae5
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-15 23:37:55 +0100

    15-02-2025: sort alphabetically desc

---
+++ postgresql/sort_desc_alphabetically.sql
@@ -0,0 +1,8 @@
+-- Alphabetically DESC
+-- To sort the table reverse alphabetically, use the DESC keyword:
+
+-- Example
+-- Sort the table by product name, in descending order:
+
+SELECT * FROM products
+ORDER BY product_name DESC;
