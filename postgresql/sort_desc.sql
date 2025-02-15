e64e3627b634fa4199db664c5157ecc4b0c64e32
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-15 23:32:19 +0100

    15-02-2025: sorting data descending order

---
+++ postgresql/sort_desc.sql
@@ -0,0 +1,8 @@
+-- DESC
+-- The ORDER BY keyword sorts the records in ascending order by default.
+-- To sort the records in descending order, use the DESC keyword.
+
+-- Example
+-- Sort the table by price, in descending order:
+SELECT * FROM products
+ORDER BY price DESC;
