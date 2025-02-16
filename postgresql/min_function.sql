54a505a3d03be9016c16020684a9fb9513b6ce56
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-16 22:34:16 +0100

    16-02-2025: min function

---
+++ postgresql/min_function.sql
@@ -0,0 +1,8 @@
+-- MIN
+-- The MIN() function returns the smallest value of the selected column.
+
+-- Example
+-- Return the lowest price in the products table:
+
+SELECT MIN(price)
+FROM products;
