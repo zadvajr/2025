6d80805ab096dbdf24f4e0f2b4544368f950b367
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-16 22:36:38 +0100

    16-02-2025: max function

---
+++ postgresql/max_function.sql
@@ -0,0 +1,8 @@
+-- MAX
+-- The MAX() function returns the largest value of the selected column.
+
+-- Example
+-- Return the highest price in the products table:
+
+SELECT MAX(price)
+FROM products;
