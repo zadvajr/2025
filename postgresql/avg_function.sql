3aab05c06974420f522828a606e0dd3238814f10
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-16 22:39:45 +0100

    16-02-2025: avg function

---
+++ postgresql/avg_function.sql
@@ -0,0 +1,8 @@
+-- AVG
+-- The AVG() function returns the average value of a numeric column.
+
+-- Example
+-- Return the average price of all the products in the products table:
+
+SELECT AVG(price)
+FROM products;
