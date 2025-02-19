e16ebf2c0b5241e30e98bad7637a4a5734b6c9b8
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-19 23:47:11 +0100

    19-02-2025: alias with space character

---
+++ postgresql/alias_space_character.sql
@@ -0,0 +1,8 @@
+-- Using Aliases With a Space Character
+-- If you want your alias to contain one or more spaces, like "My Great Products", surround your alias with double quotes.
+
+-- Example
+-- Surround your alias with double quotes:
+
+SELECT product_name AS "My Great Products"
+FROM products;
