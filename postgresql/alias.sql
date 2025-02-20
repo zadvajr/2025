4978ab59beb4ca00f9744ed7f8ad36281fc6ed71
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-02-20 23:34:34 +0100

    minor fix

--- postgresql/alias.sql
+++ postgresql/alias.sql
@@ -1,14 +1,10 @@
 -- Aliases
 -- SQL aliases are used to give a table, or a column in a table, a temporary name.
-
 -- Aliases are often used to make column names more readable.
-
 -- An alias only exists for the duration of that query.
-
 -- An alias is created with the AS keyword.
 
 -- Example
 -- Using aliases for columns:
-
 SELECT customer_id AS id
 FROM customers;
