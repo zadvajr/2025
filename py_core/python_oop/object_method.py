9c9f5636fa6dec8712c1ad099500be435b2ed165
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-03-04 18:13:02 +0100

    object method

---
+++ py_core/python_oop/object_method.py
@@ -0,0 +1,22 @@
+"""Object methods"""
+# Objects can also contain methods.
+# Methods in objects are functions that belong to the object.
+# Let's create a class with a method.
+# Example:
+class Person():
+    """A simple class to represent a person"""
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f'{self.name} is {self.age} years old'
+    
+    def info(self):
+        """Prints the name and age of the person"""
+        print(f"{self.name} is {self.age} years old")
+
+# Create an object of the class Person
+p1 = Person("Zadva", 36)
+
+# Call the method info() on the object p1
+p1.info() # Output: Zadva is 36 years old
