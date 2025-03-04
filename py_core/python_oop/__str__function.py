90bd282944ad7095160de2ffd691a640f833c346
Daniel Zadva Jnr <danielzadva@gmail.com>
2025-03-04 17:54:09 +0100

    mino fix

--- py_core/python_oop/__str__function.py
+++ py_core/python_oop/__str__function.py
@@ -4,14 +4,14 @@
 #Example
 # The string representation of an object WITHOUT the __str__() function:
 
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
 
-p1 = Person("John", 36)
+# p1 = Person("John", 36)
 
-print(p1)
+# print(p1)
 
 # Example
 # The string representation of an object WITH the __str__() function:
